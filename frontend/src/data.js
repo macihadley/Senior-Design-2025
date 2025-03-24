2842a77348a369d8cd94cc67e9df395c9e9a98d9
macihadley <53153536+macihadley@users.noreply.github.com>
2025-03-24 15:45:13 -0500

    Delete frontend directory

--- frontend/src/data.js
+++
@@ -1,8 +0,0 @@
-import titles from "./titles.json";
-
-const sections = titles.map((book) => ({
-    title: book.title, // Get title from JSON
-    content: "", // Content will be added later
-}));
-
-export default sections;
